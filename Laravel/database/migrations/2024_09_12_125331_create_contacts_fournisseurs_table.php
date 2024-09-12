8095c1c5f8ac6d44c8e606c69754b39b2404ab4e
Nicxx11 <90414449+Nicxx11@users.noreply.github.com>
2024-09-12 10:21:42 -0400

    seederFournisseurs
    exemple de fournisseur dans le seeder fournisseurs

--- Laravel/database/migrations/2024_09_12_125331_create_contacts_fournisseurs_table.php
+++ Laravel/database/migrations/2024_09_12_125331_create_contacts_fournisseurs_table.php
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('contact_fournisseur');
+        Schema::dropIfExists('contacts_fournisseurs');
     }
 };
--- Laravel/database/seeders/FournisseursSeeder.php
+++ Laravel/database/seeders/FournisseursSeeder.php
@@ -15,9 +15,16 @@ class FournisseursSeeder extends Seeder
          DB::table('fournisseurs')->insert([
              [
                 'NEQ' => '',
-                'Entreprise' => 'DeLaVie',
                 'Courriel' => 'test@test.com',
-                'MotDePasse' => Hash::make('password1234')
+                'Entreprise' => 'DeLaVie',
+                'MotDePasse' => Hash::make('password1234'),
+                'Details' => 'Beaucoup de details',
+                'No_TPS' => '321465',
+                'No_TVQ' => '9888754',
+                'Conditions_paiement' => 'Z123',
+                'Devise' => 'CAD',
+                'Mode_Communication' => 'Celluphone Tellulaire',
+                'Etat_Demande' => 'En attente',
              ]
          ]);
     }
