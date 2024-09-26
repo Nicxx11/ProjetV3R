62a3e07dd037b8beeba41fd03a0e83a5a62443e0
Nicxx11 <90414449+Nicxx11@users.noreply.github.com>
2024-09-26 10:13:08 -0400

    pageInscription
    page inscription presque entierement terminé

--- Laravel/resources/views/login/inscription.blade.php
+++ Laravel/resources/views/login/inscription.blade.php
@@ -1,54 +1,70 @@
 @extends('layouts.app')
 
-@section('titre','Inscription')
+@section('titre', 'Inscription')
+@section('newCss','css/app.css')
 
 @section('contenu')
 
-<h1>Inscription</h1>
-
-    @if (session('success'))
-        <p>{{ session('success') }}</p>
-    @endif
-
-    <form action="{{ route('inscription.store') }}" method="POST">
-        @csrf
-
-        <label for="NEQ">NEQ:</label>
-        <input type="text" id="NEQ" name="NEQ" value="{{ old('NEQ') }}">
-        @error('NEQ')
-            <p class="erreur">{{ $message }}</p>
-        @enderror
-
-        <label for="Courriel">Courriel:</label>
-        <input type="text" id="Courriel" name="Courriel" value="{{ old('Courriel') }}">
-        @error('Courriel')
-            <p class="erreur">{{ $message }}</p>
-        @enderror
-
-        <label for="Entreprise">Entreprise:</label>
-        <input type="text" id="Entreprise" name="Entreprise" value="{{ old('Entreprise') }}">
-        @error('Entreprise')
-            <p class="erreur">{{ $message }}</p>
-        @enderror
-
-        <label for="MotDePasse">Mot de Passe:</label>
-        <input type="password" id="MotDePasse" name="MotDePasse">
-        @error('MotDePasse')
-            <p class="erreur">{{ $message }}</p>
-        @enderror
-
-        <label for="MotDePasse">Confirmation Mot de Passe:</label>
-        <input type="password" id="MotDePasse_confirmation" name="MotDePasse_confirmation">
-        @error('MotDePasse')
-            <p class="erreur">{{ $message }}</p>
-        @enderror
-
-        <label for="Details">Détails:</label>
-        <textarea id="Details" name="Details">{{ old('Details') }}</textarea>
-        @error('Details')
-            <p class="erreur">{{ $message }}</p>
-        @enderror
-
-        <button type="submit">S'inscrire</button>
-    </form>
+@if (session('success'))
+    <p>{{ session('success') }}</p>
+@endif
+<div class="container-fluid vh-100 d-flex align-items-center justify-content-center overlay">
+    <div class="col-md-3">
+    </div>
+    <div class="col-md-6">
+        <div class="card cardColors text-center">
+            <form action="{{ route('inscription.store') }}" method="POST">
+                @csrf
+                <div class="form-header pb-4 pt-4">
+                    <h1>Inscription</h1>
+                </div>
+                <div class="form-group">
+                    <label for="NEQ">NEQ:</label></br>
+                    <input type="text" id="NEQ" name="NEQ" value="{{ old('NEQ') }}">
+                    @error('NEQ')
+                        <p class="erreur">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label for="Courriel">Courriel:</label></br>
+                    <input type="text" id="Courriel" name="Courriel" value="{{ old('Courriel') }}">
+                    @error('Courriel')
+                        <p class="erreur">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label for="Entreprise">Entreprise:</label></br>
+                    <input type="text" id="Entreprise" name="Entreprise" value="{{ old('Entreprise') }}">
+                    @error('Entreprise')
+                        <p class="erreur">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label for="MotDePasse">Mot de Passe:</label></br>
+                    <input type="password" id="MotDePasse" name="MotDePasse">
+                    @error('MotDePasse')
+                        <p class="erreur">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label for="MotDePasse">Confirmation Mot de Passe:</label></br>
+                    <input type="password" id="MotDePasse_confirmation" name="MotDePasse_confirmation">
+                    @error('MotDePasse')
+                        <p class="erreur">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label for="Details">Détails:</label></br>
+                    <textarea id="Details" name="Details">{{ old('Details') }}</textarea>
+                    @error('Details')
+                        <p class="erreur">{{ $message }}</p>
+                    @enderror
+                </div>
+                <button type="submit">S'inscrire</button>
+            </form>
+        </div>
+    </div>
+    <div class="col-md-3">
+    </div>
+</div>
 @endsection
