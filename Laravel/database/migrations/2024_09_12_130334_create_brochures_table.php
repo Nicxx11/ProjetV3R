e48b235c7e683559d032a831ce54a24bda536877
Nicxx11 <90414449+Nicxx11@users.noreply.github.com>
2024-12-03 14:55:09 -0500

    same as last

--- Laravel/database/migrations/2024_09_12_130334_create_brochures_table.php
+++ Laravel/database/migrations/2024_09_12_130334_create_brochures_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('brochures', function (Blueprint $table) {
             $table->id();
             $table->string('Nom', 32);  // Nom du fichier
+            $table->string('NomUnique');
             $table->string('TypeFichier');  // Type de fichier
             $table->integer('Taille');  // Taille du fichier en octets
             $table->date('DateCreation');  // Date de création
--- Laravel/resources/views/fournisseur/editProfile.blade.php
+++ Laravel/resources/views/fournisseur/editProfile.blade.php
@@ -230,8 +230,8 @@
                                 <form action="{{ route('file.upload') }}" method="POST" enctype="multipart/form-data">
                                 @csrf
                                 <div class="form-group">
-                                    <label for="file">Choisir un fichier :</label>
-                                    <input type="file" name="file" id="file" class="form-control">
+                                    <label for="brochure">Choisir un fichier :</label>
+                                    <input type="file" name="brochure" id="brochure" class="form-control">
                                 </div>
                                 <button type="submit" class="btn btn-primary">Envoyer</button>
                             </form>
